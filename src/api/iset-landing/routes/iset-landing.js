285c031e37faf1dc20543b723c873492e0e109e4
Belousov <wasik787@gmail.com>
2022-03-19 13:16:24 +0500

    test landing api

---
+++ src/api/iset-landing/routes/iset-landing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * iset-landing router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::iset-landing.iset-landing');
