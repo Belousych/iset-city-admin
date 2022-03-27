f1d1ac412710dfce0bf998406cfba015f3b8d1fb
Belousov <wasik787@gmail.com>
2022-03-27 19:16:22 +0500

    del limit

--- config/plugins.js
+++ config/plugins.js
@@ -20,8 +20,6 @@ module.exports = ({ env }) => ({
         endpoint: '/graphql',
         shadowCRUD: true,
         playgroundAlways: true,
-        depthLimit: 7,
-        amountLimit: 100,
         apolloServer: {
           tracing: false,
         },
