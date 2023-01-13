0d8bbd36ad29076e7df6f170f0cd807687492e60
Belousov <wasik787@gmail.com>
2023-01-13 20:05:15 +0500

    server prod

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,10 @@
+// module.exports = ({ env }) => ({
+//   url: env("MY_HEROKU_URL"),
+//   host: env('HOST', 'iset-city-admin-production.up.railway.app'),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
-  host: env('HOST', 'iset-city-admin-production.up.railway.app'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
 });
-// module.exports = ({ env }) => ({
-//     url: env('MY_HEROKU_URL'),
-//   });
