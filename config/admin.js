e82da34387e2783fc7ac78a47cd4af79cb266499
Belousov <wasik787@gmail.com>
2023-01-13 16:34:25 +0500

    apiToken salt test

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '5ba1427cbd648f32e1b9aa9d468a139c'),
   },
