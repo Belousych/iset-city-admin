e4480364fe09e3159b8d4167484bf6160b3b5562
Belousov <wasik787@gmail.com>
2023-01-13 17:51:06 +0500

    test

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: env("PUBLIC_URL", "https://iset-city-admin-production.up.railway.app/"),
+  url: env("PUBLIC_URL", "https://iset-city-admin-production.up.railway.app"),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
