4bf26e2ab57fc574186c03de2a13478a6a8b4af4
Belousov <wasik787@gmail.com>
2023-01-13 20:48:22 +0500

    test port

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,11 @@
-// module.exports = ({ env }) => ({
-//   url: env("MY_HEROKU_URL"),
-//   host: env('HOST', 'iset-city-admin-production.up.railway.app'),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
 module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 3001),
   url: env("MY_HEROKU_URL"),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
+// module.exports = ({ env }) => ({
+//     url: env('MY_HEROKU_URL'),
+//   });
