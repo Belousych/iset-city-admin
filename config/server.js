6957f26379af734f0c737318f2548683137fa205
Belousov <wasik787@gmail.com>
2023-01-13 17:05:14 +0500

    host 127.0.0.1

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   url: env("PUBLIC_URL", "https://iset-city-admin-production.up.railway.app/"),
   port: env.int('PORT', 1337),
   app: {
