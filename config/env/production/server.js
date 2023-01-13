da4d79ed85d1a976fdf61987456f84d6e95a9ffa
Belousov <wasik787@gmail.com>
2023-01-13 21:57:28 +0500

    aaaa

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,11 @@
-module.exports = ({ env }) => {
-  console.log("ENV", { env });
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 3001),
-    url: env("MY_HEROKU_URL"),
-    app: {
-      keys: env.array("APP_KEYS"),
-    },
-  };
-};
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL"),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
+// module.exports = ({ env }) => ({
+//     url: env('MY_HEROKU_URL'),
+//   });
