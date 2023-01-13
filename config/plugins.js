1c156f744abd959ed8f001233aef88ad4332308e
Belousov <wasik787@gmail.com>
2023-01-13 16:25:22 +0500

    users-permissions test ADMIN_JWT_SECRET

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,34 @@
 module.exports = ({ env }) => ({
-    
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
+  },
 
-    graphql: {
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 20,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-        },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 20,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
-    
-  });
+  },
+});
